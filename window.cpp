f4c48987d3b43c095bbf6af6136039a5bcea682f
Jonathan Edwards <github@jonstuff.com>
2022-01-23 21:02:05 -0800

    text

--- window.cpp
+++ window.cpp
@@ -70,86 +70,199 @@ auto    radians(double degrees)
 }
 
 
-struct BoardPixels
+struct BoardDimensions  // pixels
 {
     Gdiplus::Point  center;
-    Gdiplus::Rect   board;
 
+    struct 
+    {
+        int     outerDouble;   
+        int     innerDouble;
+        int     outerTriple;
+        int     innerTriple;
+        int     outerBullseye;
+        int     innerBullseye;
+    } radius;
 
-    int     outerDouble;   
-    int     innerDouble;
-    int     outerTriple;
-    int     innerTriple;
-    int     outerBullseye;
-    int     innerBullseye;
+    struct 
+    {
+        Gdiplus::Rect outerDouble;   
+        Gdiplus::Rect innerDouble;
+        Gdiplus::Rect outerTriple;
+        Gdiplus::Rect innerTriple;
+        Gdiplus::Rect outerBullseye;
+        Gdiplus::Rect innerBullseye;
+    } rect;
 };
 
-auto dimensions(HWND h)
+
+
+
+
+auto boardDimensions(HWND h)
 {
-    BoardPixels dimensions{};
-    RECT        client{};
+    RECT            client{};
     GetClientRect(h,&client);
 
     auto const clientWidth{client.right-client.left};
     auto const clientHeight{client.bottom-client.top};
+    auto const boardRadius   = clientWidth/2 - 50;
 
-    const auto boardRadius   = clientWidth/2 - 50;
 
-    dimensions.center.X      = clientWidth/2;
-    dimensions.center.Y      = clientWidth/2;
+    auto makeRect = [&](int radius)
+    {
+        return Gdiplus::Rect
+        {
+            clientWidth/2 - radius,
+            clientWidth/2 - radius,
+            radius*2,
+            radius*2,
+        };
+        
+    };
 
-    dimensions.board.X       = dimensions.center.X - boardRadius;
-    dimensions.board.Y       = dimensions.center.Y - boardRadius;
-    dimensions.board.Width   = boardRadius*2;
-    dimensions.board.Height  = boardRadius*2;
 
-    dimensions.outerDouble   = boardRadius;
-    dimensions.innerDouble   = static_cast<int>(dimensions.outerDouble * ( Board::Radius::innerDouble / Board::Radius::board ));
-    dimensions.outerTriple   = static_cast<int>(dimensions.outerDouble * ( Board::Radius::outerTriple / Board::Radius::board ));
-    dimensions.innerTriple   = static_cast<int>(dimensions.outerDouble * ( Board::Radius::innerTriple / Board::Radius::board ));
-    dimensions.outerBullseye = static_cast<int>(dimensions.outerDouble * ( Board::Radius::outerBullseye / Board::Radius::board ));
-    dimensions.innerBullseye = static_cast<int>(dimensions.outerDouble * ( Board::Radius::innerBullseye / Board::Radius::board ));
+    BoardDimensions dimensions
+    {
+        {clientWidth/2,clientWidth/2},
+    
+        {
+           boardRadius,
+           static_cast<int>(boardRadius * ( Board::Radius::innerDouble   / Board::Radius::board )),
+           static_cast<int>(boardRadius * ( Board::Radius::outerTriple   / Board::Radius::board )),
+           static_cast<int>(boardRadius * ( Board::Radius::innerTriple   / Board::Radius::board )),
+           static_cast<int>(boardRadius * ( Board::Radius::outerBullseye / Board::Radius::board )),
+           static_cast<int>(boardRadius * ( Board::Radius::innerBullseye / Board::Radius::board ))
+        },
+
+        {
+            makeRect(dimensions.radius.outerDouble),
+            makeRect(dimensions.radius.innerDouble),
+            makeRect(dimensions.radius.outerTriple),
+            makeRect(dimensions.radius.innerTriple),
+            makeRect(dimensions.radius.outerBullseye),
+            makeRect(dimensions.radius.innerBullseye),
+        }
+    };
 
 
     return dimensions;
 }
 
+struct RadiusDimensions // pixels
+{
+    int             angle;
+
+    Gdiplus::Point  outerDouble;   
+    Gdiplus::Point  innerDouble;
+    Gdiplus::Point  outerTriple;
+    Gdiplus::Point  innerTriple;
+    Gdiplus::Point  center;
+};
+
+
+
+auto radiusDimensions(BoardDimensions  &board, int sectorNumber)
+{
+    auto angle = Board::sector0Start + sectorNumber*Board::sectorWidth;
+
+    auto makePoint = [&](int radius)
+    {
+        return Gdiplus::Point 
+        {
+            board.center.X + static_cast<INT>(radius * std::cos(radians(angle))),
+            board.center.Y + static_cast<INT>(radius * std::sin(radians(angle)))
+        };
+    };
+
+    RadiusDimensions    dimensions
+    {
+        angle,
+        makePoint(board.radius.outerDouble),
+        makePoint(board.radius.innerDouble),
+        makePoint(board.radius.outerTriple),
+        makePoint(board.radius.innerTriple),
+        board.center
+    };
+
+    return dimensions;
+}
+
+
+auto sectorText(BoardDimensions  &board, int sectorNumber)
+{
+    auto angle = sectorNumber*Board::sectorWidth;
+
+    auto makePoint = [&](int radius)
+    {
+        return Gdiplus::PointF 
+        {
+            board.center.X + static_cast<Gdiplus::REAL>(radius * std::cos(radians(angle))),
+            board.center.Y + static_cast<Gdiplus::REAL>(radius * std::sin(radians(angle)))
+        };
+    };
+
+    return makePoint(board.radius.outerDouble+30);
+
+}
+
+
+
+
 void paint(HWND h,WPARAM w, LPARAM l)
 {
-    static Gdiplus::Pen         blackPen{Gdiplus::Color::Black};
+    static Gdiplus::Pen         blackPen  {Gdiplus::Color::Black};
+
     static Gdiplus::SolidBrush  blackBrush{Gdiplus::Color::Black};
     static Gdiplus::SolidBrush  whiteBrush{Gdiplus::Color::White};
+    static Gdiplus::SolidBrush  greenBrush{Gdiplus::Color::DarkGreen};
+    static Gdiplus::SolidBrush  redBrush  {Gdiplus::Color::DarkRed};
+
+    static Gdiplus::FontFamily  family    {L"Times New Roman"};
+    static Gdiplus::Font        font      {&family, 24, Gdiplus::FontStyleRegular, Gdiplus::UnitPixel};
 
 
-    auto dimensions{ ::dimensions(h) };
+    auto board{ ::boardDimensions(h) };
 
     PAINTSTRUCT paint;
     BeginPaint(h,&paint);
 
-    Gdiplus::Graphics   board{paint.hdc};    
+    Gdiplus::Graphics   window{paint.hdc};    
+
+    window.SetSmoothingMode(Gdiplus::SmoothingMode::SmoothingModeAntiAlias);
 
 
     for(int i=0;i<20;i++)
     {
+        auto  sectorStart { ::radiusDimensions(board,i)};
+        auto  sectorEnd   { ::radiusDimensions(board,i+1)};
+        auto  sectorText  { ::sectorText(board,i)};
+
         Gdiplus::GraphicsPath   sector;
+        Gdiplus::GraphicsPath   doubleArc;
+        Gdiplus::GraphicsPath   tripleArc;
 
-        auto startAngle = Board::sector0Start + i*Board::sectorWidth;
+        sector.AddLine(sectorStart.center  ,            sectorStart.outerDouble);
+        sector.AddArc (board.rect.outerDouble,sectorStart.angle,Board::sectorWidth);
 
-        Gdiplus::Point  end{    dimensions.center.X + static_cast<INT>(dimensions.outerDouble * std::cos(radians(startAngle))),
-                                dimensions.center.Y + static_cast<INT>(dimensions.outerDouble * std::sin(radians(startAngle)))};
+        doubleArc.AddLine(sectorStart.innerDouble, sectorStart.outerDouble);
+        doubleArc.AddArc (board.rect.outerDouble, sectorStart.angle,Board::sectorWidth);
+        doubleArc.AddLine(sectorEnd.outerDouble, sectorEnd.innerDouble);
+        doubleArc.AddArc (board.rect.innerDouble, sectorEnd.angle,-Board::sectorWidth);
 
-//        board.DrawLine(&blackPen,dimensions.center,end);
-//        board.DrawArc(&blackPen,dimensions.board,startAngle,Board::sectorWidth);
+        tripleArc.AddLine(sectorStart.innerTriple, sectorStart.outerTriple);
+        tripleArc.AddArc (board.rect.outerTriple, sectorStart.angle,Board::sectorWidth);
+        tripleArc.AddLine(sectorEnd.outerTriple, sectorEnd.innerTriple);
+        tripleArc.AddArc (board.rect.innerTriple, sectorEnd.angle,-Board::sectorWidth);
 
+        window.FillPath((i % 2) ? &blackBrush : &whiteBrush, &sector);
+        window.FillPath((i % 2) ? &redBrush   : &greenBrush, &doubleArc);
+        window.FillPath((i % 2) ? &redBrush   : &greenBrush, &tripleArc);
 
-        sector.AddLine(dimensions.center,end);
-        sector.AddArc(dimensions.board,startAngle,Board::sectorWidth);
+        window.DrawString(std::to_wstring(Board::sectorScore[i]).c_str(),-1,&font,sectorText,&blackBrush);
 
-        board.DrawPath(&blackPen, &sector);
-        board.FillPath((i % 2) ? &blackBrush : &whiteBrush, &sector);
 
     }
-
 
     
     EndPaint(h,&paint);
